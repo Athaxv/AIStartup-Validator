fa4c01aa8f94a68f33d4b3096172bd9b2c5eedc7
AtharvGaur <atharava112@gmail.com>
2025-06-18 14:44:02 +0530

    feat: add ParticlesBackground component for enhanced visual effects

---
+++ app/form/page.jsx
@@ -0,0 +1,12 @@
+import ResumeForm from '@/components/ResumeForm'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <ResumeForm/>
+    </div>
+  )
+}
+
+export default page
